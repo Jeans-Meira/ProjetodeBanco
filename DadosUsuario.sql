cea6870ca679f74291e102320bbe0bf82e182ee2
Jean Meira <jeanslima65@gmail.com>
2022-09-02 13:11:16 -0300

    Adicionar arquivos de projeto.

---
+++ DadosUsuario.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[DadosUsuario]
+(
+	[Idusuario] INT NOT NULL, 
+    [Endereco] NCHAR(255) NOT NULL, 
+    [Idadeusuario] INT NOT NULL, 
+    CONSTRAINT [FK_DadosUsuario_Usurio] FOREIGN KEY ([Idusuario]) REFERENCES [Usuario]([Id]) 
+)
---
+++ Usuario.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Usuario]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+    [NomeUsuario] varchar(255) NOT NULL, 
+    [Ativo] BIT NULL 
+)
